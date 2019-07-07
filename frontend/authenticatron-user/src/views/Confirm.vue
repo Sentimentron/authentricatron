435a2f939f53451761f3de2786df6adc1c7251b3
Richard Townsend <richard@sentimentron.co.uk>
2019-07-07 23:24:45 +0100

    feat: Adding more stuff

---
+++ frontend/authenticatron-user/src/views/Confirm.vue
@@ -0,0 +1,23 @@
+<template>
+
+    <div>
+        <b-container>
+            <b-row>
+                <b-col>
+                    <b-card>
+                        <h1>Signing in...</h1>
+                        <b-spinner variant="info" />
+                    </b-card>
+                </b-col>
+            </b-row>
+        </b-container>
+    </div>
+
+</template>
+
+<style scoped>
+div {
+    text-align: center;
+    margin-top: 10px;
+}
+</style>
